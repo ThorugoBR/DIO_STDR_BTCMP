ce17ea451bab1d6113b53f194745f63ff58e4b44
ThorugoBR <thorugo@outlook.com>
2022-06-12 06:19:37 -0300

    ok

--- Estruturas/assets/js/script.js
+++ Estruturas/assets/js/script.js
@@ -1,5 +1,25 @@
 var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber=0;
+
+// $(function() {
+// 	$(".RedOrGreen [vo]").keyup(function( e ) {
+// 		let val = currentNumberWrapper,
+// 			jElem = $(this),
+// 			addClass, removeClass;
+// 		if ( val !== "" && !isNaN( val ) ) {
+// 			val = Number( val );
+// 			addClass = ( val > 0 ? "numPos" : "numNeg");
+// 			removeClass = ( val > 0 ? "numNeg" : "numPos" );
+
+// 			jElem.addClass( addClass );
+// 			jElem.removeClass( removeClass );
+// 		}
+// 	});
+// });
+
+
+
+
 function increment(){
     currentNumber=currentNumber+1;
     currentNumberWrapper.innerHTML=currentNumber
@@ -11,3 +31,4 @@ function decrement(){
     currentNumberWrapper.innerHTML=currentNumber
 
 }
+
